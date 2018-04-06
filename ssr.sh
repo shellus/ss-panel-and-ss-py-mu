4c9041504ab5d69583b4de07b62c5100c3e58974
root <root@sg.has.cx>
2018-04-06 14:40:23 +0800

    fix "hellow"

--- ssr.sh
+++ ssr.sh
@@ -238,7 +238,7 @@ INSTALL(){
 		unzip /root/ssr_file.zip -d /root;mv /root/ss-panel-and-ss-py-mu-master/* /root
 		cp /root/ssr.sh /usr/bin/ssr;chmod 777 /usr/bin/ssr
 		rm -rf ssr_file.zip /root/ss-panel-and-ss-py-mu-master /root/picture /root/README.md /root/ssr.sh
-		clear;echo "INSTALL DONE,Hellow.";sleep 1
+		clear;echo "INSTALL DONE,Hello.";sleep 1
 	fi
 }
 
